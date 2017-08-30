9e31f673f2c77326d035da42ec34d68aa170257d
fcoedn <fcoedn@gmail.com>
2017-08-30 17:59:04 -0300

    segue

--- fimwait.cmd
+++ fimwait.cmd
@@ -5,7 +5,8 @@
 %% Data     : 03/02/94
 %% Realizar teste cherry-pick
 %%  Ja com o Commit
-%% Usando o smart-git.
+%% Usando o smart-git
+%% pelo aplicativo - 1-2
 %% ==================================================
 
 Procedure FimWait()
